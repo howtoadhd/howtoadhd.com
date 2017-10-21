3f1cea3eae785edc0284a6cf9523d79ffa43532f
Lorelei Aurora <iam@l5i.me>
2017-10-21 02:59:58 +0100

    Add Writing settings to config file.

--- app/wp-content/mu-plugins/config/writing.php
+++ app/wp-content/mu-plugins/config/writing.php
@@ -4,3 +4,15 @@ namespace HowToADHD\Config;
 
 // Disable post by email
 \add_filter( 'enable_post_by_email_configuration', '__return_false' );
+
+$settings = [
+	'default_category'    => 1, // Uncategorized
+	'default_post_format' => 0, // Standard
+	'ping_sites'          => 'http://rpc.pingomatic.com/',
+];
+
+foreach ( $settings as $setting => $value ) {
+	\add_filter( "pre_option_${setting}", function () use ( $value ) {
+		return $value;
+	} );
+}
